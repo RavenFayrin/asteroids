c63d0bcfca96a46e1b011dd976339344dc2dd545
RavenFayrin <raven.fayrin@gmail.com>
2025-10-08 15:35:28 -0400

    updated fps

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
         
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -15,8 +18,7 @@ def main():
                 return
         pygame.Surface.fill(screen, "black") #fill screen with black color
         pygame.display.flip() #refresh screen
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
-
-#test
