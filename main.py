809db195f5ceed8aec2900d4976a60547d4c5a0f
RavenFayrin <raven.fayrin@gmail.com>
2025-10-09 15:10:15 -0400

    Using Groups Instead

--- main.py
+++ main.py
@@ -5,25 +5,32 @@ from player import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
     clock = pygame.time.Clock()
-    dt = 0
     
-    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
-    #Player.draw(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+      
+    Player.containers = (updatable, drawable)
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         pygame.Surface.fill(screen, "black") #fill screen with black color
-        player.draw(screen)
-        player.update(dt)
+
+        updatable.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
+
         pygame.display.flip() #refresh screen
+
         dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
