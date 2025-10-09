cc6b50808146f1e69c4aed844ea976c7276210eb
RavenFayrin <raven.fayrin@gmail.com>
2025-10-08 21:33:38 -0400

    created visual of player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -7,7 +8,9 @@ def main():
     
     clock = pygame.time.Clock()
     dt = 0
-        
+    
+    Player.draw(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -17,6 +20,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, "black") #fill screen with black color
+        Player.draw(screen)
         pygame.display.flip() #refresh screen
         dt = clock.tick(60)/1000
 
